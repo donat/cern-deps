f94b7c49c2443473ecb707b86f31e87300d8bc00
Donat Csikos <csdonat@gmail.com>
2013-02-08 15:41:01 +0100

    Copyright was bad.

--- devtools-deps-dataserver/src/test/cern/devtools/deps/bean/AbstractBeanTest.java
+++ devtools-deps-dataserver/src/test/cern/devtools/deps/bean/AbstractBeanTest.java
@@ -1,5 +1,5 @@
 /***********************************************************************************************************************
- * © Copyright 2013 CERN. This software is distributed under the terms of the GNU General Public Licence version 3 (GPL
+ * Â© 2013 CERN. This software is distributed under the terms of the GNU General Public Licence version 3 (GPL
  * Version 3), copied verbatim in the file "COPYING". In applying this licence, CERN does not waive the privileges and
  * immunities granted to it by virtue of its status as an Intergovernmental Organization or submit itself to any
  * jurisdiction.
