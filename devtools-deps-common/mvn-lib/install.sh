32e6be18d3e1109fb20a3dd4b22db9517b0fe10b
Donat Csikos <csdonat@gmail.com>
2013-04-25 22:24:38 +0200

    target got ignored fixed install script

--- devtools-deps-common/mvn-lib/install.sh
+++ devtools-deps-common/mvn-lib/install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-﻿mvn install:install-file -Dfile=ojdbc6.jar  -DgroupId=oracle -DartifactId=ojdbc6 -Dpackaging=jar -Dversion=11.2.0.3
+mvn install:install-file -Dfile=ojdbc6.jar  -DgroupId=oracle -DartifactId=ojdbc6 -Dpackaging=jar -Dversion=11.2.0.3
 mvn install:install-file -Dfile=org.eclipse.emf.common_2.7.0.jar -DgroupId=org.eclipse.emf -DartifactId=emf-common -Dpackaging=jar -Dversion=2.7.0
 mvn install:install-file -Dfile=org.eclipse.emf.ecore_2.7.0.jar -DgroupId=org.eclipse.emf -DartifactId=emf-ecore -Dpackaging=jar -Dversion=2.7.0
 mvn install:install-file -Dfile=org.eclipse.emf.ecore.change_2.7.1.jar -DgroupId=org.eclipse.emf -DartifactId=emf-ecore-change -Dpackaging=jar -Dversion=2.7.1
