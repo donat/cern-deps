f1235bb951fd582389066345b303d9d372a8e2da
Donat Csikos <csdonat@gmail.com>
2013-06-09 22:24:05 +0200

    added test project

---
+++ devtools-deps-eclipse/devtools-deps-eclipse-test/src/cern/devtools/deps/eclipse/test/DummyTest.java
@@ -0,0 +1,18 @@
+/**
+ * Copyright (c) 2013 European Organisation for Nuclear Research (CERN), All Rights Reserved.
+ */
+
+package cern.devtools.deps.eclipse.test;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class DummyTest {
+
+    @Test
+    public void test() {
+        System.err.println("PLUGIN TESTS COME HERE!");
+        assertTrue(true);
+    }
+}
